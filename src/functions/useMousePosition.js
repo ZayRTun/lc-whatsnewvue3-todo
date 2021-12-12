bbbe7c3354249c42a922e4691df603f39a48cddb
Zayar Tun <zay.t@hotmail.com>
2021-12-12 11:20:58 +0630

    first commit

---
+++ src/functions/useMousePosition.js
@@ -0,0 +1,23 @@
+import { onMounted, onUnmounted, reactive, toRefs } from 'vue';
+
+export function useMousePosition() {
+	const pos = reactive({
+		x: 0,
+		y: 0,
+	})
+
+	function update(e) {
+		pos.x = e.pageX;
+		pos.y = e.pageY;
+	}
+
+	onMounted(() => {
+		window.addEventListener("mousemove", update);
+	});
+
+	onUnmounted(() => {
+		window.removeEventListener("mousemove", update);
+	});
+
+	return toRefs(pos)
+}
